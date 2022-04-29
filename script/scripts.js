98410044005a64deae5a2f7e75fed48d5244fd4e
Sichkar Yurii <si4kar2017@gmail.com>
2022-04-29 23:43:08 +0300

    Lesson_10

---
+++ script/scripts.js
@@ -0,0 +1,88 @@
+const TABLE_CLASS = 'table_items';
+const DELETE_BTN_CLASS = 'delete_btn';
+
+const TABLE_TEMPLATE = document.querySelector('.table_template').innerHTML;
+
+const addBtn = document.querySelector('.sending_btn');
+const inputName = document.querySelector('.input_name');
+const inputSurname = document.querySelector('.input_surname');
+const inputPhoneNumber = document.querySelector('.input_number')
+const taskList = document.querySelector('.table');
+const errorEl = document.querySelector('.error');
+
+
+addBtn.addEventListener('click', onAddTable);
+taskList.addEventListener('click', onTableElementsClick);
+
+function onAddTable(e) {
+    if (validateInput(true)) {
+        e.preventDefault();
+        showError();
+        cleanStrings();
+    } else {
+        e.preventDefault();
+        remuveError();
+        addTable();
+        cleanStrings();
+    }
+}
+
+function addTable() {
+    const taskItemHtml = getTableHTML();
+
+    taskList.insertAdjacentHTML('beforeend', taskItemHtml);
+}
+
+function getTableHTML() {
+        return TABLE_TEMPLATE.replace('{{Name}}', getInputName())
+            .replace('{{Surname}}', getInputSurname())
+            .replace('{{Phone_number}}', getInputPhoneNumber());    
+}
+
+function getInputName() {
+    return inputName.value;
+}
+
+function getInputSurname() {
+    return inputSurname.value;    
+}
+
+function getInputPhoneNumber() {
+    return inputPhoneNumber.value;
+}
+
+function validateInput() {
+    const contactName = getInputName();
+    const contactSurname = getInputSurname();
+    const contactPhoneNumber = getInputPhoneNumber();
+
+    if (contactName==='' || contactSurname==='' || contactPhoneNumber==='') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function cleanStrings() {
+    inputName.value = '';
+    inputSurname.value = '';
+    inputPhoneNumber.value = '';
+}
+
+function onTableElementsClick(e) {
+    if (e.target.classList.contains(DELETE_BTN_CLASS)) {
+        deleteContacts(e.target.closest('.' + TABLE_CLASS));
+    }
+}
+
+function showError() {
+    errorEl.classList.add('show');
+}
+
+function remuveError() {
+    errorEl.classList.remove('show');
+}
+
+function deleteContacts(el) {
+    el.remove();
+}
